979f1711c1d02c58364bfb1b4e8c48e947feb73a
linuxmobile <bdiez19@gmail.com>
2021-05-08 22:08:08 -0300

    change general package to openbox

---
+++ openbox.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Install Arch Dependencies and Packages to work everything
+yay -S rsync python psmisc xorg-xprop xorg-xwininfo imagemagick ffmpeg wireless_tools openbox \
+pulseaudio pulseaudio-alsa alsa-utils brightnessctl nitrogen dunst tint2 gsimplecal rofi qt5-styleplugins \
+lxsession xautolock rxvt-unicode-patched xclip scrot thunar thunar-archive-plugin thunar-volman \
+thunar-media-tags-plugin ffmpegthumbnailer tumbler viewnior mpv mpd mpc ncmpcpp pavucontrol \
+parcellite neofetch w3m htop picom-git obmenu-generator gtk2-perl playerctl xsettingsd \
+geany geany-plugins scrcpy sndcpy krita gimp inkscape obs-studio lxappearance file-roller nano nano-syntax-highlighting \
+obconf neovim slim colorpicker 
