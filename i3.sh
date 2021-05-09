14acf6d3c8c20a2a3724bb42e7dfa956efa312a1
linuxmobile <bdiez19@gmail.com>
2021-05-08 22:07:49 -0300

    add i3 packages

---
+++ i3.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Install Arch Dependencies and Packages to work everything
+yay -S i3-gaps alsa-utils pulseaudio pulseaudio-alsa brightnessctl psuinfo wireless_tools dunst tint2 gsimplecal rofi feh lxappearance qt5ct qt5-styleplugins lxsession xautolock rxvt-unicode-patched xclip scrot thunar thunar-archive-plugin thunar-media-tags-plugin thunar-volman lxsession tumbler jq w3m geany nano vim viewnior pavucontrol parcellite neofetch htop picom-git gtk2-perl xfce4-power-manager imagemagick playerctl networkmanager-dmenu xsettingsd
