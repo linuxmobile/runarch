ff6ed4e8fd97adf47dbdefbe9eab2e1934a61ca1
linuxmobile <bdiez19@gmail.com>
2021-05-20 21:47:58 -0300

    add kde to packages install

---
+++ kde.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Install Arch Dependencies and Packages to work everything
+
+
+yay -S plasma konsole dolphin ark spectacle krunner packagekit alsa-utils kvantum-qt5-git latte-dock appmenu-gtk module libdbusmenu-glib appmenu-qt4 libdbusmenu-gtk3 libdbusmenu-gtk2 libdbusmenu-glib pipewire pulseeffects python qt5-styleplugins viewnior mpv neofetch w3m htop scrcpy sndcpy krita gimp inkscape obs-studio nano nano-syntax-highlighting neovim gvfs gvfs-mtp exa bat firefox telegram-desktop 
