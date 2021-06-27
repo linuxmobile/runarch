9f11878737fe74d06ee17849e9289fe7cae5a5c2
linux <bdiez19@gmail.com>
2021-06-26 22:48:40 -0300

    Update kde.sh

--- kde.sh
+++ kde.sh
@@ -3,4 +3,4 @@
 # Install Arch Dependencies and Packages to work everything
 
 
-yay -S plasma konsole dolphin ark spectacle krunner packagekit alsa-utils kvantum-qt5-git latte-dock appmenu-gtk module libdbusmenu-glib appmenu-qt4 libdbusmenu-gtk3 libdbusmenu-gtk2 libdbusmenu-glib pipewire pulseeffects python qt5-styleplugins viewnior mpv neofetch w3m htop scrcpy sndcpy krita gimp inkscape obs-studio nano nano-syntax-highlighting neovim gvfs gvfs-mtp exa bat firefox telegram-desktop 
+yay -S plasma konsole kitty dolphin ark spectacle krunner packagekit alsa-utils kvantum-qt5-git latte-dock-git appmenu-gtk module libdbusmenu-glib appmenu-qt4 libdbusmenu-gtk3 libdbusmenu-gtk2 libdbusmenu-glib pipewire pulseeffects python qt5-styleplugins viewnior mpv neofetch w3m htop scrcpy sndcpy krita gimp inkscape obs-studio nano nano-syntax-highlighting neovim gvfs gvfs-mtp exa bat firefox telegram-desktop 
