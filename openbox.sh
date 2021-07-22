d2c05e9852569e0f116e248c395af2bacc70590a
linux <bdiez19@gmail.com>
2021-07-22 11:49:13 -0300

    Removed xautolock

--- openbox.sh
+++ openbox.sh
@@ -3,7 +3,7 @@
 # Install Arch Dependencies and Packages to work everything
 paru -S rsync python psmisc xorg-xprop xorg-xwininfo imagemagick ffmpeg wireless_tools openbox \
 pulseaudio pulseaudio-alsa alsa-utils brightnessctl nitrogen dunst tint2 gsimplecal rofi \
-qt5-styleplugins lxsession xautolock rxvt-unicode-truecolor-wide-glyphs xclip scrot thunar \
+qt5-styleplugins lxsession rxvt-unicode-truecolor-wide-glyphs xclip scrot thunar \
 thunar-archive-plugin thunar-volman ffmpegthumbnailer tumbler viewnior mpv mpd mpc ncmpcpp \
 pavucontrol parcellite neofetch w3m htop picom-git obmenu-generator gtk2-perl playerctl xsettingsd \
 geany geany-plugins scrcpy sndcpy krita gimp inkscape obs-studio lxappearance file-roller nano nano-syntax-highlighting \
