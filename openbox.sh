b421476d1ccced12e78260036ff73dc077612ba9
linux <bdiez19@gmail.com>
2021-08-11 14:57:23 -0300

    Remove wireless package and add better picom option

--- openbox.sh
+++ openbox.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Install Arch Dependencies and Packages to work everything
-paru -S rsync python psmisc xorg-xprop xorg-xwininfo imagemagick ffmpeg wireless_tools openbox \
+paru -S rsync python psmisc xorg-xprop xorg-xwininfo imagemagick ffmpeg openbox \
 pulseaudio pulseaudio-alsa alsa-utils brightnessctl nitrogen dunst tint2 gsimplecal rofi \
 qt5-styleplugins lxsession rxvt-unicode-truecolor-wide-glyphs xclip scrot thunar \
 thunar-archive-plugin thunar-volman ffmpegthumbnailer tumbler viewnior mpv mpd mpc ncmpcpp \
-pavucontrol parcellite neofetch w3m htop picom-git obmenu-generator gtk2-perl playerctl xsettingsd \
+pavucontrol parcellite neofetch w3m htop picom-jonaburg-git obmenu-generator gtk2-perl playerctl xsettingsd \
 geany geany-plugins scrcpy sndcpy krita gimp inkscape obs-studio lxappearance file-roller nano nano-syntax-highlighting \
 obconf neovim slim colorpicker gvfs gvfs-mtp exa bat firefox telegram-desktop pipewire-pulse
