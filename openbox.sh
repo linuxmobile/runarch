348cbb3cc6d736c5ab4d8b0d254e24d94a937ef7
linux <bdiez19@gmail.com>
2021-10-19 10:21:13 -0300

    Update openbox.sh

--- openbox.sh
+++ openbox.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Install Arch Dependencies and Packages to work everything
-paru -S rsync python psmisc xorg-xprop xorg-xwininfo imagemagick ffmpeg openbox \
-pulseaudio pulseaudio-alsa alsa-utils brightnessctl nitrogen dunst tint2 gsimplecal rofi \
-qt5-styleplugins lxsession rxvt-unicode-truecolor-wide-glyphs xclip scrot thunar \
-thunar-archive-plugin thunar-volman ffmpegthumbnailer tumbler viewnior mpv mpd mpc ncmpcpp \
-pavucontrol parcellite neofetch w3m htop picom-jonaburg-git obmenu-generator gtk2-perl playerctl xsettingsd \
+paru -S python psmisc xorg-xprop xorg-xwininfo imagemagick ffmpeg wireless_tools openbox \
+pulseaudio pulseaudio-alsa alsa-utils brightnessctl nitrogen dunst tint2 lxsession       \
+rxvt-unicode-truecolor-wide-glyphs xclip scrot mpd mpc thunar thunar-archive-plugin      \
+thunar-volman ffmpegthumbnailer tumbler w3m ncmpcpp viewnior mpv pavucontrol parcellite  \
+gsimplecal neofetch htop xsettingsd obmenu-generator perl-gtk3 picom-git rofi rsync playerctl \
 geany geany-plugins scrcpy sndcpy krita gimp inkscape obs-studio lxappearance file-roller nano nano-syntax-highlighting \
-obconf neovim slim colorpicker gvfs gvfs-mtp exa bat firefox telegram-desktop pipewire-pulse
+obconf neovim slim colorpicker gvfs gvfs-mtp exa bat firefox telegram-desktop pipewire-pulse spotify 
