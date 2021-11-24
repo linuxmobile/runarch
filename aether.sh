5bf88e5d0ba6c4ca7b85e75c17fa3156d9e45552
linuxmobile ( リナックス ) <bdiez19@gmail.com>
2021-11-23 23:38:39 -0300

    Aether Openbox

---
+++ aether.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Install Arch Dependencies and Packages to work everything
+paru -S openbox psuinfo wireless_tools dunst tint2 gsimplecal rofi feh lxappearance \
+qt5ct qt5-styleplugins lxsession rxvt-unicode-truecolor-wide-glyphs ffmpeg ffmpegthumbnailer \
+xclip scrot thunar thunar-archive-plugin thunar-media-tags-plugin thunar-volman mpv xsettingsd \
+tumbler jq w3m geany nano vim viewnior pavucontrol parcellite neofetch obmenu-generator perl-gtk3 \
+htop picom-git gtk2-perl imagemagick playerctl networkmanager-dmenu rsync xsettingsd imagemagick psmisc \
+xorg-xprop xorg-xwininfo python geany geany-plugins krita gimp inkscape lxappearance file-roller nano nano-syntax-highlighting \
+obconf slim colorpicker gvfs gvfs-mtp exa bat firefox telegram-desktop pipewire-pulse spotify microsoft-edge-beta-bin 
+
