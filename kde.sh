c64a24b5fe31cef9d3cf41e69daa92440f19d50e
linuxmobile ( リナックス ) <bdiez19@gmail.com>
2021-11-25 11:38:18 -0300

    Update minimal installation

--- kde.sh
+++ kde.sh
@@ -3,4 +3,7 @@
 # Install Arch Dependencies and Packages to work everything
 
 
-yay -S plasma konsole kitty dolphin ark spectacle krunner packagekit alsa-utils kvantum-qt5-git latte-dock-git appmenu-gtk module libdbusmenu-glib appmenu-qt4 libdbusmenu-gtk3 libdbusmenu-gtk2 libdbusmenu-glib pipewire pulseeffects python qt5-styleplugins viewnior mpv neofetch w3m htop scrcpy sndcpy krita gimp inkscape obs-studio nano nano-syntax-highlighting neovim gvfs gvfs-mtp exa bat firefox telegram-desktop 
+paru -S plasma-desktop konsole kitty kate dolphin ark spectacle krunner packagekit alsa-utils kvantum-qt5-git \
+appmenu-gtk module libdbusmenu-glib appmenu-qt4 libdbusmenu-gtk3 libdbusmenu-gtk2 libdbusmenu-glib pipewire \
+pulseeffects python qt5-styleplugins viewnior mpv neofetch w3m htop gimp inkscape nano nano-syntax-highlighting \
+gvfs gvfs-mtp exa firefox telegram-desktop microsoft-edge-beta-bin sddm plasma-nm plasma-pa kdeplasma-addons kde-gtk-config plasma-wayland-session
