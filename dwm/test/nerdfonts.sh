5ef432f5f9e177c5baff9b86f07b8e84149dc32b
linuxmobile ( リナックス ) <bdiez19@gmail.com>
2022-01-27 17:03:50 -0300

    Create nerdfonts.sh

---
+++ dwm/test/nerdfonts.sh
@@ -0,0 +1,15 @@
+sleep 1
+    echo -e "$prefix Downloading Nerd Fonts..."
+    echo -e "$prefix If this don't work, delete this posix!"
+    
+    if [[ -d $HOME/Donwloads/ ]]; then
+        mkdir -p $HOME/Downloads/nerdfonts/
+        wget https://github.com/ryanoasis/nerd-fonts/releases/download/2.2.0-RC/CascadiaCode.zip
+        wget https://github.com/ryanoasis/nerd-fonts/releases/download/2.2.0-RC/Iosevka.zip
+        wget https://github.com/ryanoasis/nerd-fonts/releases/download/2.2.0-RC/JetBrainsMono.zip
+        wget https://github.com/ryanoasis/nerd-fonts/releases/download/2.2.0-RC/Noto.zip
+        unzip *.zip $HOME/Downloads/nerdfonts/
+    fi
+    
+    echo -e "$prefix nerdfonts downloaded!"
+    sleep 0.7
