06e1f473e52f42f23e56fbac28ef69f92f46c758
linuxmobile ( リナックス ) <bdiez19@gmail.com>
2022-01-27 10:46:02 -0300

    Remove unused scripts

--- i3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Install Arch Dependencies and Packages to work everything
-paru -S python ffmpeg pulseaudio pulseaudio-alsa alsa-utils dunst xclip scrot     \\
-thunar thunar-archive-plugin thunar-volman ffmpegthumbnailer tumbler w3m          \\
-viewnior mpv neofetch htop lxappearance picom-jonaburg-git rofi rsync pavucontrol \\
-ranger python-pip noto-fonts-emoji noto-fonts-cjk python-pillow-git xwallpaper    \\
-exa bat file-roller geany geany-plugins gvfs gvfs-mtp htop kitty wal-git imlib2   \\
-xorg-xsetroot cascaydia-code-fonts nerd-fonts-jetbrains-mono nerd-fonts-noto
