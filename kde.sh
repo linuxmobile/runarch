d62c926f636567e090ae7610c0bc04863ffb69a4
linuxmobile ( リナックス ) <bdiez19@gmail.com>
2022-01-27 10:46:14 -0300

    Remove unused scripts

--- kde.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Install Arch Dependencies and Packages to work everything
-
-
-paru -S plasma-desktop konsole kitty kate dolphin ark spectacle krunner packagekit alsa-utils kvantum-qt5-git \
-appmenu-gtk module libdbusmenu-glib appmenu-qt4 libdbusmenu-gtk3 libdbusmenu-gtk2 libdbusmenu-glib pipewire \
-pulseeffects python qt5-styleplugins viewnior mpv neofetch w3m htop gimp inkscape nano nano-syntax-highlighting \
-gvfs gvfs-mtp exa firefox telegram-desktop microsoft-edge-beta-bin sddm plasma-nm plasma-pa kdeplasma-addons kde-gtk-config plasma-wayland-session
