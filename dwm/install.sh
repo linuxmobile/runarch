cf1d5a75729aa85d3673974b33f70b569e84a0cf
linuxmobile ( リナックス ) <bdiez19@gmail.com>
2022-01-30 10:49:12 -0300

    Fix 🩹: Improved install.sh

--- dwm/install.sh
+++ dwm/install.sh
@@ -14,13 +14,27 @@ banner() {
       '
 }
 
+installParu() {
+    clear
+    
+    sleep 1
+    echo -e "$prefix Install paru as AUR helper..."
+    echo -e "$prefix Installing paru..."
+    
+        git clone https://aur.archlinux.org/paru.git $HOME/Downloads/.installer
+        cd $HOME/Downloads/.installer/ && makepkg -si
+    
+    echo -e "$prefix Paru installed!"
+    sleep 0.7
+}
+
 downloadDependencies() {
     if grep "Arch\|Artix\|EndeavourOS\|Manjaro" /etc/*-release; then
         clear 
         echo -e "$prefix Running an system update..."
         sudo pacman --noconfirm -Syu
 
-        mkdir -p $HOME/.setup-scripto
+        mkdir -p $HOME/Downloads/.installer
         sleep 0.5
         clear
 
@@ -50,8 +64,8 @@ downloadDependencies() {
 
             case $paru in
                 [yY][*])
-                    git clone https://aur.archlinux.org/paru.git $HOME/.setup-scripto
-                    (cd $HOME/.setup-scripto && makepkg -si)
+                    git clone https://aur.archlinux.org/paru.git $HOME/Downloads/.installer
+                    (cd $HOME/Downloads/.installer && makepkg -si)
 
                     echo -e "$prefix paru installed. Installing dependencies..."
                     paru -S python ffmpeg pipewire pipewire-alsa pipewire-pulse alsa-utils dunst       \
@@ -84,10 +98,9 @@ installZSH() {
     echo -e "$prefix Install Oh-My-Zsh..."
     echo -e "$prefix If this don't work, delete this posix."
     
-    if [[ -d $HOME/.oh-my-zsh/ ]]; then
-        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    fi
-    
+            cd $HOME/
+            sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+        
     echo -e "$prefix Oh-My-Zsh installed!"
     sleep 0.7
 }
@@ -99,11 +112,9 @@ clonePlugins() {
     echo -e "$prefix Install Oh-My-Zsh plugins..."
     echo -e "$prefix If this don't work, delete this posix too."
     
-    if [[ -d $HOME/.oh-my-zsh/ ]]; then
-        git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-        git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-        git clone --depth 1 https://github.com/zsh-users/zsh-completions.git ${ZSH_CUSTOM:~/.oh-my-zsh/custom}/plugins/zsh-completions
-    fi
+        git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+        git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+        git clone --depth 1 https://github.com/zsh-users/zsh-completions.git $HOME/.oh-my-zsh/custom/plugins/zsh-completions
     
     echo -e "$prefix Oh-My-Zsh plugins installed!"
     sleep 0.7
@@ -116,14 +127,14 @@ installNerdFonts() {
     echo -e "$prefix Downloading Nerd Fonts..."
     echo -e "$prefix If this don't work, delete this posix!"
     
-    if [[ -d $HOME/Donwloads/ ]]; then
         mkdir -p $HOME/Downloads/nerdfonts/
+        cd $HOME/Downloads/nerdfonts
         wget https://github.com/ryanoasis/nerd-fonts/releases/download/2.2.0-RC/CascadiaCode.zip
         wget https://github.com/ryanoasis/nerd-fonts/releases/download/2.2.0-RC/Iosevka.zip
         wget https://github.com/ryanoasis/nerd-fonts/releases/download/2.2.0-RC/JetBrainsMono.zip
         wget https://github.com/ryanoasis/nerd-fonts/releases/download/2.2.0-RC/Noto.zip
         unzip *.zip $HOME/Downloads/nerdfonts/
-    fi
+        rm -rf *.zip
     
     echo -e "$prefix nerdfonts downloaded!"
     sleep 0.7
@@ -136,9 +147,7 @@ copyFonts() {
     echo -e "$prefix Copying fonts..."
     echo -e "$prefix If this don't work, delete this posix."
     
-    if [[ -d $HOME/Downloads/nerdfonts/ ]]; then
         sudo cp -R $HOME/Downloads/nerdfonts/ /usr/share/fonts/
-    fi
     
     echo -e "$prefix nerd fonts installed!"
     sleep 0.7
@@ -151,50 +160,32 @@ copyFiles() {
     echo -e "$prefix Syncing files..."
     echo -e "$prefix Will make a sync if dwm, config and scripts doesn't exists."
 
-    if [[ -d $HOME/ ]]; then
+        cd $HOME/Downloads/
+        git clone https://github.com/linuxmobile/dwm-dots
+        cd $HOME/Downloads/dwm-dots
         rsync -avxHAXP --exclude '.git*' .* ~/
-    fi
 
     echo -e "$prefix Synced all configs files!"
     sleep 0.7
 
-
-
-    if [[ -d $HOME/lnxdwm/ ]]; then
-        mkdir $HOME/lnxdwm_backup/ && mv $HOME/lnxdwm/* $HOME/lnxdwm_backup/
-        cp -r ./lnxdwm/* $HOME/lnxdwm/
-    else
-        mkdir $HOME/lnxdwm && cp -r ./lnxdwm/* $HOME/lnxdwm/
-    fi
+        cd $HOME/Downloads/dwm-dots
+        mkdir $HOME/lnxdwm/ && cp -r ./lnxdwm/* $HOME/lnxdwm/
 
     echo -e "$prefix Copied DWM configuration!"
     sleep 0.7
 
-    if [[ -d $HOME/lnxdwm/ ]]; then
-        sudo make install
-    else
-        mkdir $HOME/lnxdwm && cp -r ./lnxdwm/* $HOME/lnxdwm/ && sudo make install
-    fi
+        cd $HOME/lnxdwm/ && sudo make install
 
     echo -e "$prefix Make DWM magic!"
     sleep 0.7
 
-    if [[ -d $HOME/st/ ]]; then
-        mkdir $HOME/st_backup && mv $HOME/st/* $HOME/st_backup/
-        cp -r ./st/* $HOME/st/
-    else
+        cd $HOME/Downloads/dwm-dots
         mkdir $HOME/st/ && cp -r ./st/* $HOME/st/
-    fi
 
     echo -e "$prefix Copied st configuration!"
     sleep 0.7
 
-    if [[ -d $HOME/st/ ]]; then
-        mkdir $HOME/st_backup && mv $HOME/st/* $HOME/st_backup/
-        cp -r ./st/* $HOME/st/
-    else
-        mkdir $HOME/st/ && cp -r ./st/* $HOME/st/ && sudo make install
-    fi
+        cd $HOME/st/ && sudo make install
 
     echo -e "$prefix Make ST happen!"
 
@@ -251,7 +242,7 @@ welcome() {
 
 success() {
     # Remove the custom directory made by the script
-    rm -rf $HOME/.setup-scripto
+    rm -rf $HOME/Downloads/.installer
 
     whiptail --title "$title" \
         --msgbox "Setup success. Please reboot xorg if you are on an active session." 10 60
@@ -272,6 +263,9 @@ welcome || fuckUser
 
 # Fancy banner
 banner
+
+# Install paru
+installParu
 
 # Download dependencies
 downloadDependencies
