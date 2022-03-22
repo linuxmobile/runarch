b77e5c067d796894b086c803473167ec6729e505
linuxmobile ( リナックス ) <bdiez19@gmail.com>
2022-03-22 07:51:26 -0300

    Update install.sh

--- dwm/install.sh
+++ dwm/install.sh
@@ -100,10 +100,12 @@ installZSH() {
     sleep 1
     echo -e "$prefix Install Oh-My-Zsh..."
     echo -e "$prefix If this don't work, delete this posix."
-    
+    	    if [[ -e $HOME/.oh-my-zsh/ ]]; then
+	    clear
+	    else
             cd $HOME/
             sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-            exit
+	    fi
         
     echo -e "$prefix Oh-My-Zsh installed!"
     sleep 0.7
